3bf74e561248dfc31af28c5d172cbb7d064c30f1
Jukka Kuutti <jukka_kuutti@hotmail.com>
2021-03-17 12:30:49 +0200

    1.7

--- osa1/unicafe/unicafe/src/App.js
+++ osa1/unicafe/unicafe/src/App.js
@@ -6,6 +6,26 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const sum = good * 1 + neutral * 0 + bad * -1
+  const avarage = sum / total
+  const positive = good / total * 100
+
+  return (
+  <div>
+    <h1>statistics</h1>
+    <p>good {total}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {total}</p>
+    <p>avarage {avarage}</p>
+    <p>positive {positive} %</p>
+  </div>
+  )
+}
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -18,10 +38,8 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="Good"/>
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text="Bad"/>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics good = {good} neutral = {neutral} bad = {bad}></Statistics>
+      
     </div>
   )
 }
