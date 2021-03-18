6d2007c62e2b9fc4bca4276d2ab025c28e0b0fa8
Jukka Kuutti <jukka_kuutti@hotmail.com>
2021-03-18 10:38:05 +0200

    2.4

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -27,11 +27,8 @@ const Content = ({parts}) => {
 
 
 const Total = ({parts}) => {
-//let sum = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
-console.log(parts);
-const reducer = (accumulator, currentValue) => accumulator + currentValue["exercises"];
+const reducer = (accumulator, currentValue) => accumulator + currentValue.exercises;
 let sum = parts.reduce(reducer,0);
-console.log("sum:", sum);
 
 return (
     <>
@@ -44,7 +41,6 @@ const Course = ({course}) => (
         <Header name={course.name}/>
         <Content parts={course.parts}/>
         <Total parts={course.parts}/>
-        
     </div>
 )
 
