6ebfa8227c9fe2e763ef81188a72a1052ad849b8
Jukka Kuutti <jukka_kuutti@hotmail.com>
2021-03-23 13:46:05 +0200

    2.14

--- osa2/maiden_tiedot/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/maiden_tiedot/src/App.js
@@ -1,23 +1,61 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const Weather = ({city}) => {
+  const [temperature, setTemp ] = useState('')
+  const [wind, setWind ] = useState('')
+  const [weathericon, setWeatherIcon ] = useState('')
+
+  useEffect(() => {
+    const api_key = process.env.REACT_APP_API_KEY
+    console.log("api", api_key);
+    console.log('weather effect')
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${city}`)
+      .then(response => {
+        console.log('promise fulfilled')
+        const current = response.data.current
+        setTemp(current.temperature)
+        setWind(current.wind_speed + " mph direction " + current.wind_dir)
+        setWeatherIcon(current.weather_icons)
+        console.log(response.data);
+      })
+  }, [city])
+
+  return (
+    <div>
+      <h2>Weather in {city}</h2>
+      <p>temp: {temperature}</p>
+      <p>wind: {wind}</p>
+      <img src={weathericon} alt=""></img>
+    </div>
+  )
+}
+
+const Country = ({country}) => {
+  return (
+    <div>
+          <h1>{country.name}</h1>
+          <p>capital {country.capital}</p>
+          <p>population {country.population}</p>
+          <h2>languages</h2>
+          <ul>
+          {country.languages.map(language => 
+            <li key={language.iso639_2}>{language.name}</li>
+            )}
+          </ul>
+          <img width="100" src={country.flag} alt="flag"/>
+          <Weather city={country.capital}></Weather>
+      </div>
+  )
+}
+
 const Countries = ({countries, onChangeFunction}) => {
   console.log(onChangeFunction)
   if(countries.length === 1)
   {
     return (
-      <div>
-          <h1>{countries[0].name}</h1>
-          <p>capital {countries[0].capital}</p>
-          <p>population {countries[0].population}</p>
-          <h2>languages</h2>
-          <ul>
-          {countries[0].languages.map(language => 
-            <li key={language.iso639_2}>{language.name}</li>
-            )}
-          </ul>
-          <img width="100" src={countries[0].flag} alt="flag"/>
-      </div>
+      <Country country={countries[0]}></Country>
     )
 
   }
@@ -29,7 +67,6 @@ const Countries = ({countries, onChangeFunction}) => {
       </ul>
     )
   }
-  
   else
   {
     return (
@@ -42,9 +79,10 @@ const Countries = ({countries, onChangeFunction}) => {
   }
 }
 
+
 const App = () => {
   const [countries, setCountries] = useState([])
-  const [ filterValue, setFilterValue ] = useState('')
+  const [filterValue, setFilterValue ] = useState('')
 
   useEffect(() => {
     console.log('effect')
@@ -67,9 +105,7 @@ const App = () => {
   return(
     <div>
       find countries<input value={filterValue} onChange={handleFilterChange}></input>
-      <div>
-        <Countries countries={countriesToShow} onChangeFunction={() => handleFilterChange}></Countries>
-      </div>
+      <Countries countries={countriesToShow} onChangeFunction={() => handleFilterChange}></Countries>
     </div>
     )
 }
