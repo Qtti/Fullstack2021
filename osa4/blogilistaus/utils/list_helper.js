1cc5415aaf54d53fe6cc571ea4f86c132c7c60b8
Jukka Kuutti <jukkakuutti@talentree.fi>
2021-04-16 10:14:14 +0300

    4.5

--- osa4/blogilistaus/utils/list_helper.js
+++ osa4/blogilistaus/utils/list_helper.js
@@ -8,7 +8,29 @@ const totalLikes = (blogs) => {
   
   return total
 }
+
+const favoriteBlog = (blogs) => {
+
+  const sort = blogs.sort(function (a, b) {
+    return a.likes - b.likes
+  })
+  
+  let max = sort[sort.length - 1]
+  if(max)
+  {
+    delete max._id
+    delete max.__v
+    delete max.url
+  }
+  else
+  {
+    max = []
+  }
+
+  return max
+}
+
   
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
